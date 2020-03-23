42211a33a92ba3e5ac90db5469e1b5dc4a6e6278
Venky-8 <mangnalev.1999@gmail.com>
2020-03-23 19:59:31 +0530

    Added QR Code detection

--- app/src/main/java/com/example/android/sggstiec/MainActivity.java
+++ app/src/main/java/com/example/android/sggstiec/MainActivity.java
@@ -1,31 +1,18 @@
 package com.example.android.sggstiec;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import com.camerakit.CameraKitView;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.ml.vision.FirebaseVision;
-import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
-import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
-import com.google.firebase.ml.vision.common.FirebaseVisionImage;
-import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
-
-import java.util.List;
 
 import im.delight.android.location.SimpleLocation;
 
@@ -34,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
     private static final String TAG = "MainActivity";
     private SimpleLocation location;
-    private CameraKitView cameraKitView;
+    private Button scan_btn;
+    public static  TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +30,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         location = new SimpleLocation(this);
-        cameraKitView = findViewById(R.id.camera);
 
-        findViewById(R.id.someView).setOnClickListener(new View.OnClickListener() {
+        scan_btn = findViewById(R.id.scan_button);
+        result = findViewById(R.id.result_text);
+
+        marshmallowGPSPremissionCheck();
+
+        scan_btn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -54,70 +46,34 @@ public class MainActivity extends AppCompatActivity {
 //                    // ask the user to enable location access
 //                    SimpleLocation.openSettings(MainActivity.this);
 //                }
-                checkLocationPermission();
                 final double latitude = location.getLatitude();
                 final double longitude = location.getLongitude();
 
                 // TODO
                 Toast.makeText(MainActivity.this, "Latitude: " + latitude + "Longitude: " + longitude, Toast.LENGTH_SHORT).show();
                 float distanceInMeters = getDistance(latitude, longitude);
-                if(distanceInMeters <= 100) {
-                    setContentView(R.layout.camera_view);
-                    scanQRCode();
+                if (distanceInMeters <= 100) {
+                    //TODO: CAMERA
+                    startActivity(new Intent(getApplicationContext(), ScanCodeActivity.class));
+                } else {
+                    Toast.makeText(MainActivity.this, "You are not near TIEC Lab", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), ScanCodeActivity.class));
                 }
             }
 
         });
     }
 
-    private void scanQRCode() {
-        cameraKitView.captureImage(new  CameraKitView.ImageCallback() {
-            @Override
-            public void onImage(CameraKitView cameraKitView, final byte[] capturedImage) {
-                // capturedImage contains the image from the CameraKitView.
-                FirebaseVisionImageMetadata metadata = new FirebaseVisionImageMetadata.Builder()
-                        .setWidth(480)   // 480x360 is typically sufficient for
-                        .setHeight(360)  // image recognition
-                        .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
-                        .setRotation(FirebaseVisionImageMetadata.ROTATION_0)
-                        .build();
-
-                FirebaseVisionImage image = FirebaseVisionImage.fromByteArray(capturedImage, metadata);
-                FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance()
-                        .getVisionBarcodeDetector();
-
-                Task<List<FirebaseVisionBarcode>> result = detector.detectInImage(image)
-                        .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
-                            @Override
-                            public void onSuccess(List<FirebaseVisionBarcode> barcodes) {
-                                // Task completed successfully
-                                // ...
-                                for (FirebaseVisionBarcode barcode: barcodes) {
-                                    String code = barcode.getDisplayValue();
-                                    Toast.makeText(MainActivity.this, "Scanned QR Code: " + code, Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                // Task failed with an exception
-                                // ...
-                            }
-                        });
-            }
-        });
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        // make the device update its location
-        location.beginUpdates();
-
-        // ...
-    }
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//
+//        // make the device update its location
+//        marshmallowGPSPremissionCheck();
+//        location.beginUpdates();
+//
+//        // ...
+//    }
 
     @Override
     protected void onPause() {
@@ -146,116 +102,28 @@ public class MainActivity extends AppCompatActivity {
         return distanceInMeters;
     }
 
-    public boolean checkLocationPermission() {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-                new AlertDialog.Builder(this)
-                        .setTitle(R.string.title_location_permission)
-                        .setMessage(R.string.text_location_permission)
-                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                //Prompt the user once explanation has been shown
-                                ActivityCompat.requestPermissions(MainActivity.this,
-                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                                        MY_PERMISSIONS_REQUEST_LOCATION);
-                            }
-                        })
-                        .create()
-                        .show();
-
-
-            } else {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_LOCATION);
-            }
-            return false;
+    private void marshmallowGPSPremissionCheck() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && this.checkSelfPermission(
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && this.checkSelfPermission(
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.CAMERA}
+                            , MY_PERMISSIONS_REQUEST_LOCATION);
         } else {
-            return true;
+            //   gps functions.
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // location-related task you need to do.
-                    if (ContextCompat.checkSelfPermission(this,
-                            Manifest.permission.ACCESS_FINE_LOCATION)
-                            == PackageManager.PERMISSION_GRANTED) {
-
-                        //Request location updates:
-//                        locationManager.requestLocationUpdates(provider, 400, 1, this);
-                        recreate();
-                    }
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-
-                }
-                return;
-            }
-
+        if (requestCode == MY_PERMISSIONS_REQUEST_LOCATION
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            //  gps functionality
         }
     }
 
-//    /**
-//     * Get the angle by which an image must be rotated given the device's current
-//     * orientation.
-//     */
-//    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-//    private int getRotationCompensation(String cameraId, Activity activity, Context context)
-//            throws CameraAccessException {
-//        // Get the device's current rotation relative to its "native" orientation.
-//        // Then, from the ORIENTATIONS table, look up the angle the image must be
-//        // rotated to compensate for the device's rotation.
-//        int deviceRotation = activity.getWindowManager().getDefaultDisplay().getRotation();
-//        int rotationCompensation = ORIENTATIONS.get(deviceRotation);
-//        // On most devices, the sensor orientation is 90 degrees, but for some
-//        // devices it is 270 degrees. For devices with a sensor orientation of
-//        // 270, rotate the image an additional 180 ((270 + 270) % 360) degrees.
-//        CameraManager cameraManager = (CameraManager) context.getSystemService(CAMERA_SERVICE);
-//        int sensorOrientation = cameraManager
-//                .getCameraCharacteristics(cameraId)
-//                .get(CameraCharacteristics.SENSOR_ORIENTATION);
-//        rotationCompensation = (rotationCompensation + sensorOrientation + 270) % 360;
-//        // Return the corresponding FirebaseVisionImageMetadata rotation value.
-//        int result;
-//        switch (rotationCompensation) {
-//            case 0:
-//                result = FirebaseVisionImageMetadata.ROTATION_0;
-//                break;
-//            case 90:
-//                result = FirebaseVisionImageMetadata.ROTATION_90;
-//                break;
-//            case 180:
-//                result = FirebaseVisionImageMetadata.ROTATION_180;
-//                break;
-//            case 270:
-//                result = FirebaseVisionImageMetadata.ROTATION_270;
-//                break;
-//            default:
-//                result = FirebaseVisionImageMetadata.ROTATION_0;
-//                Log.e(TAG, "Bad rotation value: " + rotationCompensation);
-//        }
-//        return result;
-//    }
 }
---
+++ app/src/main/java/com/example/android/sggstiec/ScanCodeActivity.java
@@ -0,0 +1,40 @@
+package com.example.android.sggstiec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScanCodeActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    ZXingScannerView mScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        MainActivity.result.setText(rawResult.getText());
+        onBackPressed();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+}
